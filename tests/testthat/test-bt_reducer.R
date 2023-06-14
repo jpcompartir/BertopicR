097ad34dead076f73e1ce19b70fb8ec918b3b3a6
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-06-14 17:29:26 +0100

    commit beginning of bt_reducer test suite (one of these days I'll make sure to fully follow the TDD philosophy and write the tests before the features!)

--- tests/testthat/test-bt_reducer.R
+++ tests/testthat/test-bt_reducer.R
@@ -1,3 +1,23 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+#Testing this is actually pretty sophisticated, so first cover the named arguments, then ellipsis separately, then an output.
+
+test_that("bt_reducer stops when it's given bad arguments",{
+
+  #Make an empty array to feed into the mandatory argument (which should be array or df)
+  array <- array()
+  expect_error(bt_reducer(embeddings = array, return_value = "reduces"), "should be one of")
+  expect_error(bt_reducer(embeddings = array, n_neighbors = "15"), "is.numeric.*n_neighb")
+  expect_error(bt_reducer(embeddings = array, n_components = TRUE), "is.numeric.*n_components")
+  expect_error(bt_reducer(embeddings = array, min_dist = "12"))
+  expect_error(bt_reducer(embeddings = array, random_state = "42"), "is.numeric.*random_state")
+  expect_error(bt_reducer(embeddings = array, metric = 2), "is.character.*metric" )
 })
+
+test_that("bt_reducer accepts an array or a data frame as embeddings", {
+
+})
+
+test_that("bt_reducer's return_value argument is working as expected", {
+
+})
+
+#How to test for side effects, e.g. test for the printing of the progress bar and other text when bt_reducer is called?
