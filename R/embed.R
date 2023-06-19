c508bc247c1331ae106da353264a8bd2a8af79d5
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-06-19 12:32:45 +0100

    split bt_embed into bt_make_embedder() bt_do_embedding()

--- R/embed.R
+++ R/embed.R
@@ -1,3 +1,36 @@
+#' Make an embedding model
+#'
+#' Initially this function is built upon the `sentence_transformers` Python library, but it may be expanded to accept other frameworks. You should feed in your documents as a list. You can use hardware accelerators e.g. GPUs, to speed up computation.
+#'
+#' @param model_name Name of embedding model as a string (not case sensitive)
+#'
+#' @return a Python object
+#' @export
+#'
+#' @examples
+#' emebdder <- bt_make_embedder("all-mpnet-base-v2")
+#'
+#' embedder <- bt_make_embedder("aLL-minilm-l6-v2")
+bt_make_embedder <- function(model_name) {
+
+  #Can leave space for a second argument, which is model_source and then use switch() to allow for embedding models other than sentence_transformers if the need arises.
+
+  if(!is.character(model_name)){
+    stop("'model_name' should be a string of text e.g. 'all-mpnet-base-v2")
+  }
+
+   #Import sentence transformers to embed documents. In the future we might want to use an argument + switch to allow the user to use other platforms for embeddings.
+  sentence_transformers <- reticulate::import("sentence_transformers")
+
+  #Instantiate embedding model, pass ellipsis here
+  embedder <- sentence_transformers$SentenceTransformer(model_name_or_path = model_name)
+
+  attr(embedder, "embedding_model") <- model_name
+
+  return(embedder)
+}
+
+
 #' Embed your documents
 #'
 #' Takes a document, or list of documents, and returns a numerical embedding which can be used as features for machine learning model or for semantic similarity search. If you have pre-computed your embeddings you can skip this step. the bt_embed function is designed to be used as one step in a topic modelling pipeline.
@@ -15,38 +48,84 @@
 #'
 #' @return An array of floating point numbers
 #' @export
-#'
-bt_embed <- function(documents, ..., embedding_model = "all-MiniLM-L6-v2", accelerator = "mps", progress_bar = TRUE) {
+bt_do_embedding <- function(embedder, documents, ..., accelerator = "mps", progress_bar = TRUE) {
 
-  #Input validation, stop early if the conditions aren't met, could add some `...` stuff here with the dots 2 list pattern
-  stopifnot(is.character(embedding_model),
-            is.character(accelerator) | is.null(accelerator),
+
+  if(!grepl("^sentence_tran",class(embedder)[[1]])){
+    stop("embedder should be a sentence transformers model")
+  }
+
+  stopifnot(is.character(accelerator) | is.null(accelerator),
             is.logical(progress_bar))
 
-  #Import sentence transformers to embed documents. In the future we might want to use an argument + switch to allow the user to use other platforms for embeddings.
-  sentence_transformers <- reticulate::import("sentence_transformers")
-
-  #Instantiate embedding model, pass ellipsis here
-  st_model <-
-    sentence_transformers$SentenceTransformer(model_name_or_path = embedding_model)
-
   embeddings <-
-    st_model$encode(
+    embedder$encode(
       documents,
       device = accelerator,
       show_progress_bar = progress_bar,
       ... #This allows knowledgeable users to include other arguments, without bloating the autofill for inexperienced users
-      )
+    )
 
   #Give the user a quick console nudge that the embeddings are ready
   message("\nEmbedding proccess finished")
-
-  #Leave a trace on the embeddings to extract which model they were from. Not 100% sure this is the right way to go, but seems like it should work. Can grab it later with attributes(embeddings)[["embeedding_model"]]
-  attr(embeddings, "embedding_model") <- embedding_model
 
   #Keep track of the number of documents that were fed into the bt_embed function, should be useful later when merging data frames and documents aren't present any more. Should we just return a data frame with the documents, and nested embeddings?
   n_documents <- length(documents)
   attr(embeddings, "n_documents") <- n_documents
 
   return(embeddings)
+
 }
+
+# Hide old implementation ----
+#' Embed your documents
+#'
+#' Takes a document, or list of documents, and returns a numerical embedding which can be used as features for machine learning model or for semantic similarity search. If you have pre-computed your embeddings you can skip this step. the bt_embed function is designed to be used as one step in a topic modelling pipeline.
+#'
+#'
+#' The function currently returns an object with two additional attributes: embedding_model, n_documents, they have been appended to the embeddings for extraction at later steps in the pipeline, e.g. when merging data frames later on it's important to check how many documents we entered.
+#'
+#'
+#' #' @param documents A character vector of the documents to be embedded, e.g. your text variable
+#' #' @param ... Optional or additional parameters passed to SentenceTransformer's encode function, e.g. batch_size
+#' #' @param embedding_model A string containing the name of the SentenceTransformers embedding model
+#' #' @param accelerator A string containing the name of a hardware accelerator, e.g. "mps", "cuda". If NULL no acceleartor is used.
+#' #' @param progress_bar A logical value indicating whether a progress bar is shown in the console
+#' #'
+#' #' @return An array of floating point numbers
+#' #' @export
+#' #'
+#' bt_embed <- function(documents, ..., embedding_model = "all-MiniLM-L6-v2", accelerator = "mps", progress_bar = TRUE) {
+#'
+#'   #Input validation, stop early if the conditions aren't met, could add some `...` stuff here with the dots 2 list pattern
+#'   stopifnot(is.character(embedding_model),
+#'             is.character(accelerator) | is.null(accelerator),
+#'             is.logical(progress_bar))
+#'
+#'   #Import sentence transformers to embed documents. In the future we might want to use an argument + switch to allow the user to use other platforms for embeddings.
+#'   sentence_transformers <- reticulate::import("sentence_transformers")
+#'
+#'   #Instantiate embedding model, pass ellipsis here
+#'   st_model <-
+#'     sentence_transformers$SentenceTransformer(model_name_or_path = embedding_model)
+#'
+#'   embeddings <-
+#'     st_model$encode(
+#'       documents,
+#'       device = accelerator,
+#'       show_progress_bar = progress_bar,
+#'       ... #This allows knowledgeable users to include other arguments, without bloating the autofill for inexperienced users
+#'     )
+#'
+#'   #Give the user a quick console nudge that the embeddings are ready
+#'   message("\nEmbedding proccess finished")
+#'
+#'   #Leave a trace on the embeddings to extract which model they were from. Not 100% sure this is the right way to go, but seems like it should work. Can grab it later with attributes(embeddings)[["embeedding_model"]]
+#'   attr(embeddings, "embedding_model") <- embedding_model
+#'
+#'   #Keep track of the number of documents that were fed into the bt_embed function, should be useful later when merging data frames and documents aren't present any more. Should we just return a data frame with the documents, and nested embeddings?
+#'   n_documents <- length(documents)
+#'   attr(embeddings, "n_documents") <- n_documents
+#'
+#'   return(embeddings)
+#' }
