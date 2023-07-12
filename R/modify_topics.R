aed5d6f7447915603fdde028c8f34be4ed490fcf
AoifeRyan-sc <125266574+AoifeRyan-sc@users.noreply.github.com>
2023-07-12 18:08:02 +0100

    added reduce outliers functions and tests

--- R/modify_topics.R
+++ R/modify_topics.R
@@ -89,13 +89,65 @@ bt_merge_topics <- function(model = model,
   return(model)
 } 
 
-bt_reduce_outliers <- function(model = model,
-                            documents,
-                            probabilities = NULL,
-                            method = c("probabilities", 
-                                       "distributions",
-                                       "ctfidf",
-                                       "embeddings")){
+# bt_distribute_outliers <- function(model = model,
+#                             documents,
+#                             topics,
+#                             probability_matrix = NULL,
+#                             threshold,
+#                             embeddings = NULL,
+#                             method = c("probabilities", 
+#                                        "distributions",
+#                                        "ctfidf",
+#                                        "embeddings")){
+#   #Input validation
+#   if(!grepl("^bertopic", methods::S3Class(model)[[1]])){
+#     stop("model should be a BERTopic model")
+#   }
+#   
+#   if(is.null(model$topics_)){
+#     stop("BERTopic model is not fitted, use bt_fit_model to fit.")
+#   }
+#   
+#   if(!is.null(probabilities) & !is.list(probabilties)){
+#     stop("probabilities must be a matrix") 
+#   }
+#   
+#   if(!method %in% c("probabilities", 
+#                     "distributions",
+#                     "ctfidf",
+#                     "embeddings")){
+#     stop("method must be one of: probabilties, distributions, ctf-idf or embeddings") 
+#   }
+#   
+#   
+#   # what do I want to return from this? 
+#   new_topics <- model$reduce_outliers(documents = documents, 
+#                                       topics = topics,
+#                                       probabilities = probability_matrix,
+#                                       strategy = method,
+#                                       threshold = threshold)
+#   return(model)
+# } 
+
+
+#' Title
+#'
+#' @param model 
+#' @param documents 
+#' @param topics 
+#' @param probability_matrix 
+#' @param threshold 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+bt_outliers_probs <- function(model = model,
+                                     documents,
+                                     topics,
+                                     probability_matrix,
+                                     threshold){
+  
   #Input validation
   if(!grepl("^bertopic", methods::S3Class(model)[[1]])){
     stop("model should be a BERTopic model")
@@ -105,28 +157,148 @@ bt_reduce_outliers <- function(model = model,
     stop("BERTopic model is not fitted, use bt_fit_model to fit.")
   }
   
-  if(!is.null(probabilities) & !is.list(probabilties)){
-    stop("probabilities must be a list") 
+  if(!is.null(probability_matrix) & !is.matrix(probability_matrix)){
+    stop("topic-document probabilitiy matrix must be a matrix") 
   }
   
-  if(!method %in% c("probabilities", 
-                    "distributions",
-                    "ctfidf",
-                    "embeddings")){
-    stop("method must be one of: probabilties, distributions, ctf-idf or embeddings") 
+  stopifnot(length(topics) == length(documents), # same number of topics as docs
+            dim(probability_matrix)[1] == length(documents), # same number of docs 
+            dim(probability_matrix)[2] == length(unique(topics)) - 1, # same number of topics
+            is.character(documents), 
+            is.numeric(threshold))
+  
+  ## validation finished ##
+  
+  
+  new_topics <- model$reduce_outliers(documents = documents,
+                                      topics = topics,
+                                      probabilities = probability_matrix,
+                                      strategy = "probabilities",
+                                      threshold = threshold)
+
+  new_topics_matched <- data.frame(message = documents,
+                                   old_topics = topics,
+                                   new_topics = new_topics)
+  
+  return(new_topics_matched)
+}
+
+
+#' Title
+#'
+#' @param model 
+#' @param documents 
+#' @param topics 
+#' @param embeddings 
+#' @param threshold 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+bt_outliers_embeddings <- function(model = model,
+                                          documents,
+                                          topics,
+                                          embeddings,
+                                          threshold){
+  #Input validation
+  if(!grepl("^bertopic", methods::S3Class(model)[[1]])){
+    stop("model should be a BERTopic model")
   }
   
-  # covert probabilties to integers 
-  topics_to_merge_int <- list()
-  for (i in 1:length(topics_to_merge)) {
-    topics_to_merge_int[[i]] <- as.integer(topics_to_merge[[i]])
+  if(is.null(model$topics_)){
+    stop("BERTopic model is not fitted, use bt_fit_model to fit.")
   }
   
-  # convert to python list
-  topics_to_merge <- reticulate::r_to_py(topics_to_merge)
+  stopifnot(length(topics) == length(documents), # same number of topics as docs
+            nrow(embeddings) == length(documents), # same number of topics
+            is.array(embeddings)| is.data.frame(embeddings) | is.null(embeddings),
+            is.character(documents),
+            is.numeric(threshold))
   
-  model$merge_topics(documents,
-                     topics_to_merge_int)
+  ## validation finished ##
   
-  return(model)
-} 
+  
+  new_topics <- model$reduce_outliers(documents = documents,
+                                      topics = topics,
+                                      embeddings = embeddings,
+                                      strategy = "embeddings",
+                                      threshold = threshold)
+  
+  new_topics_matched <- data.frame(message = documents,
+                                   old_topics = topics,
+                                   new_topics = new_topics)
+  
+  return(new_topics_matched)
+  
+}
+
+#' Title
+#'
+#' @param model 
+#' @param documents 
+#' @param topics 
+#' @param threshold 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+bt_outliers_tokenset_similarity <- function(...,
+                                            model = model,
+                                            documents,
+                                             topics,
+                                             window,
+                                             stride,
+                                             threshold){
+  ##### NEED TO DO MORE WORK HERE ON ... #####
+  #Input validation
+  if(!grepl("^bertopic", methods::S3Class(model)[[1]])){
+    stop("model should be a BERTopic model")
+  }
+  
+  if(is.null(model$topics_)){
+    stop("BERTopic model is not fitted, use bt_fit_model to fit.")
+  }
+  
+  stopifnot(length(topics) == length(documents), # same number of topics as docs
+            nrow(embeddings) == length(documents), # same number of topics
+            is.array(embeddings)| is.data.frame(embeddings) | is.null(embeddings),
+            is.character(documents),
+            is.numeric(threshold))
+  
+  ## validation finished ##
+  
+  
+  new_topics <- model$reduce_outliers(documents = documents,
+                                      topics = topics,
+                                      embeddings = embeddings,
+                                      strategy = "embeddings",
+                                      threshold = threshold)
+  
+  new_topics_matched <- data.frame(message = documents,
+                                   old_topics = topics,
+                                   new_topics = new_topics)
+  
+  return(new_topics_matched)
+}
+
+
+#' Title
+#'
+#' @param model 
+#' @param documents 
+#' @param topics 
+#' @param threshold 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+bt_outliers_ctfidf <- function(model = model,
+                                      documents,
+                                      topics,
+                                      threshold){
+  
+  
+}
--- tests/testthat/test-bt_modify_topics.R
+++ tests/testthat/test-bt_modify_topics.R
@@ -1,37 +1,37 @@
 test_that("bt_merge_topics only accepts correct objects", {
-  
-  bt <- reticulate::import("bertopic") 
+
+  bt <- reticulate::import("bertopic")
   sentences <- stringr::sentences[1:200]
   model <- bt$BERTopic()
   model$fit(sentences)
   model_unfitted <- bt$BERTopic()
-  
+
   # model detection is working:
   expect_error(bt_merge_topics(model = "test",
                                documents = sentences,
                                topics_to_merge = list(-1,0)),
                regexp = "model should be a BERTopic model")
-  
-  
+
+
   # fitted model detection is working:
   expect_error(bt_merge_topics(model = model_unfitted,
                                documents = sentences,
                                topics_to_merge = list(-1,0)),
                regexp = "BERTopic model is not fitted, use bt_fit_model to fit.")
-  
+
   # topics to merge in correct format:
   expect_error(bt_merge_topics(model = model,
                                documents = sentences,
                                topics_to_merge = c(-1,0)),
                regexp = "topics_to_merge must be a list or where you want to perform multiple merges, a list of lists/vectors")
-  
-  
+
+
   # only accepts documents in correct form:
   expect_error(bt_merge_topics(model = model,
                                documents = c(-1,0),
                                topics_to_merge = list(-1,0)),
                regexp = "documents must be of type character")
-  
+
   # accepts correct inputs
   expect_silent(bt_merge_topics(model = model,
                                 documents = sentences,
@@ -39,22 +39,22 @@ test_that("bt_merge_topics only accepts correct objects", {
 })
 
 test_that("bt_merge_topics merges topics", {
-  
+
   bt <- reticulate::import("bertopic") # do I need to run this again?
   sentences <- stringr::sentences[1:200]
   model <- bt$BERTopic()
   model$fit(sentences)
-                  
+
   # merges two topics:
   expect_equal(nrow(model$get_topic_info()),
                bt_merge_topics(model = model,
                                documents = sentences,
-                               topics_to_merge = list(-1,0))$get_topic_info() %>% 
+                               topics_to_merge = list(-1,0))$get_topic_info() %>%
                  nrow() + 1)
-  
+
   model <- bt$BERTopic()
   model$fit(sentences)
-  
+
   # merges multiple topics:
   expect_equal(nrow(model$get_topic_info()),
                bt_merge_topics(model = model,
@@ -63,3 +63,90 @@ test_that("bt_merge_topics merges topics", {
                                                       c(1, 2)))$get_topic_info() %>%
                  nrow() + 2)
 })
+
+
+test_that("bt_outlier_probs only accepts correct objects", {
+  
+  bt <- reticulate::import("bertopic") 
+  sentences <- stringr::sentences[1:100]
+  model <- bt$BERTopic()
+  model$fit(sentences)
+  model_unfitted <- bt$BERTopic()
+  n_topics <- model$get_topic_info() %>% length()
+  
+  # model detection is working:
+  expect_error(bt_outliers_probs(model = model_unfitted,
+                                 documents = sentences,
+                                 topics = list(rep(1, 100)),
+                                 probability_matrix = matrix(data = rep(1, 200), nrow = 100),
+                                 threshold = 0.1),
+               regexp = "BERTopic model is not fitted, use bt_fit_model to fit.")
+  
+  expect_error(bt_outliers_probs(model = "text",
+                                 documents = sentences,
+                                 topics = list(rep(1, 100)),
+                                 probability_matrix = matrix(data = rep(1, 200), nrow = 100),
+                                 threshold = 0.1),
+               regexp = "model should be a BERTopic model")
+  
+  # matrix detection is working:
+  expect_error(bt_outliers_probs(model = model,
+                                 documents = sentences,
+                                 topics = list(rep(1, 100)),
+                                 probability_matrix = list(100),
+                                 threshold = 0.1),
+               regexp = "topic-document probabilitiy matrix must be a matrix")
+  
+  
+  # checks correct number of topics in matrix
+  expect_error(bt_outliers_probs(model = model,
+                                 documents = sentences,
+                                 topics = replicate(100, 1),
+                                 probability_matrix = matrix(data = rep(1, 100*(n_topics-2)), ncol = n_topics-2),
+                                 threshold = 0.1))
+  
+  # checks correct number of documents in matrix
+  expect_error(bt_outliers_probs(model = model,
+                                 documents = sentences,
+                                 topics = list(rep(1, 100)),
+                                 probability_matrix = matrix(data = rep(1, 50*(n_topics-1)), ncol = n_topics-1),
+                                 threshold = 0.1))
+
+  # 
+  # accepts correct inputs
+  expect_silent(bt_outliers_probs(model = model,
+                                 documents = sentences,
+                                 topics = sample(1:n_topics, 100, replace = TRUE),
+                                 probability_matrix = matrix(data = rep(1, 100*(n_topics-1)), ncol = n_topics-1),
+                                 threshold = 0.1))
+
+})
+
+test_that("bt_outliers_probs returns correct output", {
+  
+  bt <- reticulate::import("bertopic") # do I need to run this again?
+  sentences <- stringr::sentences[1:100]
+  model <- bt$BERTopic()
+  model$fit(sentences)
+  n_topics <- model$get_topic_info() %>% length()
+  
+  # returns df with document, old topics, new topics:
+  expect_silent(bt_outliers_probs(model = model,
+                                  documents = sentences,
+                                  topics = sample(1:n_topics, 100, replace = TRUE),
+                                  probability_matrix = matrix(runif(100 * (n_topics-1)), nrow = 100),
+                                  threshold = 0.1))
+  
+  # model <- bt$BERTopic()
+  # model$fit(sentences)
+  # 
+  # # merges multiple topics:
+  # expect_equal(nrow(model$get_topic_info()),
+  #              bt_merge_topics(model = model,
+  #                              documents = sentences,
+  #                              topics_to_merge = list(c(-1, 0),
+  #                                                     c(1, 2)))$get_topic_info() %>%
+  #                nrow() + 2)
+})
+
+
