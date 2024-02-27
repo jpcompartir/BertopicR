f71446c14b589309f49a897c7f0ca05fec055327
AoifeRyan-sc <125266574+AoifeRyan-sc@users.noreply.github.com>
2024-02-27 10:06:32 +0000

    removed test data, removed comments in clustering_ui (need to remove altogether), corrected spelling mistake in reduce.R

--- R/clustering_ui.R
+++ R/clustering_ui.R
@@ -11,10 +11,11 @@ clustering_ui <- function(id) {
       conditionalPanel(
         condition = "input.cluster_method == 'HDBSCAN'",
         sliderInput("min_cluster_size", "Minimum cluster size:",
-                    min = 2, max = nrow(df)/2, value = 20), # arbitrarily setting the defaults
+                    # min = 2, max = nrow(df)/2, value = 20), # arbitrarily setting the defaults
+                    min = 2, max = 100, value = 20), # arbitrarily setting the defaults
         sliderInput("min_sample_size", "Minimum number of samples:",
                     min = 1, max = 10, value = 1), # these values update as defined in the server
-        # radioButtons("hdbscan_metric", "Metric", choices = c("cosine", "euclidean")),
+        radioButtons("hdbscan_metric", "Metric", choices = c("cosine", "euclidean")),
         selectInput("hdbscan_metric", "Metric", choices = c(
           # "cosine",
           "braycurtis", "canberra", "chebyshev", "cityblock", "correlation",  "dice", "euclidean", "hamming", "jaccard", "jensenshannon", "kulczynski1", "mahalanobis", "matching", "minkowski", "rogerstanimoto", "russellrao", "seuclidean", "sokalmichener", "sokalsneath", "sqeuclidean", "yule")),
